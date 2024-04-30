430a7a4088bef4d29048315c078e42c0ae70be30
Kanisshka <kanisshka@Kanisshkas-Laptop.local>
2024-04-30 11:55:20 +0530

    firebase

--- src/app/(DashboardLayout)/layout/vertical/sidebar/AuthRoute.js
+++ src/app/(DashboardLayout)/layout/vertical/sidebar/AuthRoute.js
@@ -1,14 +1,23 @@
 import { useRouter } from 'next/navigation';
 import { useSelector } from 'react-redux';
-
+import { useEffect } from 'react';
 const AuthRoute = ({ children }) => {
   const router = useRouter();
-  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated); // Use useSelector directly
 
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.replace('/auth/auth1/login');
+    }
+  }, [isAuthenticated, router]);
+  
   if (!isAuthenticated) {
-    return router.replace('/auth/auth1/login');
-    
+    return null; // Prevent rendering children if not authenticated
   }
+  // if (!isAuthenticated) {
+  //   router.replace('/auth/auth1/login');
+  //   return null;
+  // }
 
   return children;
 };
